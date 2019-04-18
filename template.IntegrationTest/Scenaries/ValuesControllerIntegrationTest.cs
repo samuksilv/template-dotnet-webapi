e6e4f6670a21c336974f3b9503b7a7064ccc5333
samuksilv <samuel.silv.carvalho@gmail.com>
2019-04-18 00:13:21 -0300

    adiçao de pacotes e de teste de integraçao

---
+++ template.IntegrationTest/Scenaries/ValuesControllerIntegrationTest.cs
@@ -0,0 +1,36 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using template.API;
+using template.IntegrationTest.Feature;
+using Xunit;
+
+namespace template.IntegrationTest.Scenaries {
+    public class ValuesControllerIntegrationTest : IClassFixture<AppFactory<Startup>>{
+
+        private readonly HttpClient _client;
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public ValuesControllerIntegrationTest (AppFactory<Startup> factory) {
+            // this._client = new HttpClient();
+            // this._client.BaseAddress = factory.ClientOptions.BaseAddress;
+            this._factory= factory;
+            this._client= this._factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task CanGetValues () {
+            // The endpoint or route of the controller action.
+            var httpResponse = await _client.GetAsync ("/api/values");
+
+            // Must be successful.
+            httpResponse.EnsureSuccessStatusCode ();
+
+            // Deserialize and examine results.
+            var stringResponse = await httpResponse.Content.ReadAsStringAsync ();
+            var values = JsonConvert.DeserializeObject (stringResponse);
+            Assert.NotNull (values);
+        }
+    }
+}
